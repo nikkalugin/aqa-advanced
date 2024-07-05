c701cea37075c417bdc8b46801b8d58c21eeb984
LAPTOP-P9HH53JB\exped <expeditiontomysoul@gmail.com>
2024-07-05 15:44:52 +0300

    Homework lection 4 is done

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let FirstPerson = 'Nik Johnson';
+let SecondPerson = 'Vicky Birmingham'
+
+let greeting = 'Welcome, ' + FirstPerson + ' and ' + SecondPerson + '!';
+console.log(greeting);
+
+let greeting1 = `Welcome, ${FirstPerson} and ${SecondPerson}!`;
+console.log(greeting1);
---
+++ jsBasics/task4.js
@@ -0,0 +1,23 @@
+// Task 4.1
+
+const PI = Math.PI;
+let radius = 10;
+let Circle = PI * radius ** 2;
+
+console.log('Circle Area is ' + Circle.toFixed(2));
+
+// Task 4.2
+
+const length = 17;
+let width = 20.5;
+let Rectangle = length * width;
+
+console.log('Rectangle Area is ' + Rectangle.toFixed(2));
+
+// Task 4.3
+
+const radius1 = 10;
+let height = 10;
+let Cylinder = PI * radius1 ** 2 * height;
+
+console.log('Cylinder Area is ' + Cylinder.toFixed(2));
