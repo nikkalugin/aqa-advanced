c246909eff5030e754548c818856516c2195abce
LAPTOP-P9HH53JB\exped <expeditiontomysoul@gmail.com>
2024-08-16 11:00:17 +0300

    task for husky

---
+++ task1.js
@@ -0,0 +1,61 @@
+class ToDoService {
+    async getToDo(id) {
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+            if (!response.ok) {
+                throw new Error('Network response is out of work');
+            }
+            return await response.json();
+        } catch (error) {
+            console.error('Error fetching todo: ', error);
+            throw error;
+        }
+    }
+}
+
+class UserService {
+    async getUser(id) {
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+            if (!response.ok) {
+                throw new Error('Network response is out of work');
+            }
+            return await response.json();
+        } catch (error) {
+            console.error('Error fetching user: ', error);
+            throw error;   
+
+        }
+    }
+}
+
+const toDoService = new ToDoService();
+const userService = new UserService();
+
+async function fetchDataWithPromiseAll() {
+    try {
+        const [toDo, user] = await Promise.all([
+            toDoService.getToDo(2),
+            userService.getUser(6),
+        ]);
+        console.log('ToDo: ', toDo);
+        console.log('User: ', user);
+    } catch (error) {
+        console.error('Error fetching data: ', error);
+    }
+}
+
+async function fetchDataWithPromiseRace() {
+    try {
+        const result = await Promise.race([
+            toDoService.getToDo(3),
+            userService.getUser(5),
+        ]);
+        console.log('First resolved promise: ', result);
+    } catch (error) {
+        console.error('Error fetching data: ', error);
+    }
+}
+
+fetchDataWithPromiseAll();
+fetchDataWithPromiseRace();
